251959c17be265cb4337bb04548601708ae5af61
John <jddhalogamer@gmail.com>
2021-02-03 17:31:30 -0600

    added GET HTTP path

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'May',
+        email: 'may@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Brandon',
+        email: 'brandon@gmail.com',
+        status: 'inactive',
+    },
+    {
+        id: 3,
+        name: 'Misty',
+        email: 'misty@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
