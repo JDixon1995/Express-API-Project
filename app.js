568a1d27b64a8bf10bc91ce71fc233b4e366d5f5
John <jddhalogamer@gmail.com>
2021-02-03 18:39:45 -0600

    added PUT and DELETE methods

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const moment = require('moment');
 const logger = require('./middleware/logger');
 
@@ -10,25 +9,15 @@ const app = express();
 // Init Middleware
 // app.use(logger);
 
-//Get single member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if(found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id} found`});
-    }
-
-    
-})
-
-// Gets Members
-app.get('/api/members', (req, res) => res.json(members));
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Members API routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
